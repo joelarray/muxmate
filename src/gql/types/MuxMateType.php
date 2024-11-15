55c6dd667ed17084c945adec04b77a90c3a12674
joelarray <joel@joelbaker.net>
2024-11-15 02:59:46 +0000

    update for craft5

--- src/gql/types/MuxMateType.php
+++ src/gql/types/MuxMateType.php
@@ -9,7 +9,13 @@
 namespace vaersaagod\muxmate\gql\types;
 
 use vaersaagod\muxmate\models\MuxMateFieldAttributes;
+use vaersaagod\muxmate\helpers\MuxMateHelper;
+use vaersaagod\muxmate\models\MuxPlaybackId;
+use vaersaagod\muxmate\MuxMate;
+
 use craft\gql\base\ObjectType;
+
+use Illuminate\Support\Collection;
 
 use GraphQL\Type\Definition\ResolveInfo;
 
@@ -21,11 +27,19 @@ class MuxMateType extends ObjectType
     protected function resolve(mixed $source, array $arguments, mixed $context, ResolveInfo $resolveInfo): mixed
     {
         $fieldName = $resolveInfo->fieldName;
-
+        
         switch ($fieldName) {
             case 'playbackId':
-                if ($source instanceof MuxMateFieldAttributes && $source->muxPlaybackId) {
-                    return $source->muxPlaybackId;
+                if ($source instanceof MuxMateFieldAttributes && $source->muxMetaData) {
+                    $playbackId = Collection::make($source->muxMetaData['playback_ids'] ?? [])
+                        ->where('policy', MuxMate::getInstance()->getSettings()->defaultPolicy)
+                        ->first();
+                    
+                    if (!$playbackId) {
+                        return null;
+                    }
+
+                    return new MuxPlaybackId($playbackId);
                 }
                 return null;
                 break;
